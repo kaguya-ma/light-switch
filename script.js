da8ce86fd1fc209ad9d8bd6191a8919263007ff5
Ma Tester <lulamae1983@gmail.com>
2020-10-09 09:40:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function lightOn() {
+  document.querySelector(".title").style.color="black"
+  document.querySelector(".container")
+  .style.backgroundColor="white"
+   document.querySelector("html")
+  .style.backgroundColor="white"
+
+
+}
+
+
+function lightOff() {
+  document.querySelector(".title").style.color="white"
+  document.querySelector(".container")
+  .style.backgroundColor="black"
+   document.querySelector("html")
+  .style.backgroundColor="black"
+}
